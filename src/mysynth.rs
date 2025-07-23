2191e18d04c8d18764cc509e331d0eefdebbd7ec
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-23 22:00:04 +0200

    make mysynth parameterized by a heuristic

--- src/mysynth.rs
+++ src/mysynth.rs
@@ -4,6 +4,10 @@ fn delta(x: usize, y: usize) -> usize {
     if x > y { x-y } else { y-x }
 }
 
+fn heur_cost(l: &[CounterExample], r: &[CounterExample]) -> usize {
+    delta(l.len(), r.len())
+}
+
 pub fn mysynth(ces: &[CounterExample], vars: &[Symbol]) -> P {
     if ces.is_empty() {
         return P::Var(vars[0]);
@@ -19,26 +23,33 @@ pub fn mysynth(ces: &[CounterExample], vars: &[Symbol]) -> P {
     let mut best_y = vars[0];
     let mut best_cost = usize::MAX;
 
+    let mut best_l = Vec::new();
+    let mut best_r = Vec::new();
+
     for x in vars {
         for y in vars {
-            let pair = (x, y);
-            let m = ces.iter().filter(|ce| ce.sigma[x] < ce.sigma[y]).count();
-            let n = ces.len();
-            let cost = delta(2*m, n);
+            let mut l = Vec::new();
+            let mut r = Vec::new();
+            for ce in ces.iter() {
+                if ce.sigma[x] < ce.sigma[y] {
+                    l.push(ce.clone());
+                } else {
+                    r.push(ce.clone());
+                }
+            }
+            let cost = heur_cost(&l, &r);
             if cost < best_cost {
                 best_x = *x;
                 best_y = *y;
                 best_cost = cost;
+                best_l = l;
+                best_r = r;
             }
         }
     }
 
-    let (x, y) = (best_x, best_y);
-    let l: Vec<_> = ces.iter().filter(|ce| ce.sigma[&x] < ce.sigma[&y]).cloned().collect();
-    let r: Vec<_> = ces.iter().filter(|ce| ce.sigma[&x] >= ce.sigma[&y]).cloned().collect();
-    
-    let l = mysynth(&l, vars);
-    let r = mysynth(&r, vars);
+    let l = mysynth(&best_l, vars);
+    let r = mysynth(&best_r, vars);
 
-    P::IfLt(Box::new([P::Var(x), P::Var(y), l, r]))
+    P::IfLt(Box::new([P::Var(best_x), P::Var(best_y), l, r]))
 }
